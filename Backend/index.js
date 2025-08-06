a512e26adb67a67170100c27cec5a0f993dcc515
Agent-cat <2300031042@kluniversity.in>
2025-08-06 23:21:21 +0530

    index.js

--- Backend/index.js
+++ Backend/index.js
@@ -30,7 +30,7 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-// Error handling middleware
+
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({
